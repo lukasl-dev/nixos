ea8d422c92ef9a614f7c5acd2fd956e2332cc689
Lukas Leeb <git@lukasl.dev>
2024-06-17 01:00:54 +0200

    Add trusted users

--- hosts/vega/default.nix
+++ hosts/vega/default.nix
@@ -31,6 +31,11 @@
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
+
+    trusted-users = [
+      "root"
+      "lukas"
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
