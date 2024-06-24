52b23033c0c11ffdde1ce7c090de976295b382d1
Lukas Leeb <git@lukasl.dev>
2024-06-24 20:14:14 +0200

    Update date formatting in ags

--- home/hyprland/window-rules.nix
+++ home/hyprland/window-rules.nix
@@ -9,6 +9,12 @@
       "float,title:(Save File)"
       "float,title:(Open File)"
       "float,initialTitle:(discord popout)"
+
+      "opacity 0.0 override,class:^(xwaylandvideobridge)$"
+      "noanim,class:^(xwaylandvideobridge)$"
+      "noinitialfocus,class:^(xwaylandvideobridge)$"
+      "maxsize 1 1,class:^(xwaylandvideobridge)$"
+      "noblur,class:^(xwaylandvideobridge)$"
     ];
   };
 }
