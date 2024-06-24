ece8743b7d8a6cf29e10d8f58b0897f2e3b5af46
Lukas Leeb <git@lukasl.dev>
2024-06-24 17:50:11 +0200

    Set brave as default

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -1,5 +1,8 @@
 { pkgs, config, ... }:
 
+let
+browser = [ "brave.desktop" ];
+in
 {
   xdg = {
     enable = true;
@@ -19,10 +22,31 @@
         common.default = [ "gtk" ];
         hyprland.default = [
           "gtk"
-          "hyprland"
+            "hyprland"
         ];
       };
       extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     };
-  };
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/x-extension-htm" = browser;
+        "application/x-extension-html" = browser;
+        "application/x-extension-shtml" = browser;
+        "application/x-extension-xht" = browser;
+        "application/x-extension-xhtml" = browser;
+        "application/xhtml+xml" = browser;
+        "application/json" = browser;
+        "application/pdf" = browser;
+        "text/html" = browser;
+        "x-scheme-handler/about" = browser;
+        "x-scheme-handler/chrome" = browser;
+        "x-scheme-handler/ftp" = browser;
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+        "x-scheme-handler/unknown" = browser;
+      };
+    };
+  };  
 }
