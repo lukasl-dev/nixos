67ba4f5d752bc01e9b5a024d66d7260fc9879856
Lukas Leeb <git@lukasl.dev>
2024-06-24 19:27:04 +0200

    Use catppuccin mocha for dunst

--- home/dunst/default.nix
+++ home/dunst/default.nix
@@ -1,3 +1,33 @@
 {
-  services.dunst.enable = true;
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        width = 300;
+        height = 300;
+        offset = "30x50";
+        origin = "top-right";
+        transparency = 10;
+        frame_color = "#89b4fa";
+        separator_color = "#89b4fa";
+        font = "Droid Sans 9";
+      };
+
+      urgency_low = {
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+      };
+
+      urgency_normal = {
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+      };
+
+      urgency_critical = {
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+        frame_color = "#fab387";
+      };
+    };
+  };
 }
