2da3874d6e3b6ca341c007790b6f2357b3d6fd66
Lukas Leeb <git@lukasl.dev>
2024-08-21 13:00:58 +0200

    Update signs

--- dots/nvim/lua/plugins/gitsigns.lua
+++ dots/nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,23 @@
 return {
   "lewis6991/gitsigns.nvim",
 
-   event = "BufRead",
-   cmd = "Gitsigns",
+  event = "BufRead",
+  cmd = "Gitsigns",
 
-   opts = {},
+  opts = {
+    signs = {
+      add = { text = "" },
+      change = { text = "" },
+      delete = { text = "󰍵" },
+      changedelete = { text = "󱕖" },
+      untracked = { text = "" },
+    },
+    signs_staged = {
+      add = { text = "" },
+      change = { text = "" },
+      delete = { text = "󰍵" },
+      changedelete = { text = "󱕖" },
+      untracked = { text = "" },
+    },
+  },
 }
--- dots/nvim/lua/plugins/lspconfig.lua
+++ dots/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,20 @@
+vim.fn.sign_define(
+  "DiagnosticSignError",
+  { text = "", texthl = "DiagnosticSignError" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignWarn",
+  { text = "", texthl = "DiagnosticSignWarn" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignInfo",
+  { text = "", texthl = "DiagnosticSignInfo" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignHint",
+  { text = "", texthl = "DiagnosticSignHint" }
+)
+
 local function on_attach(_, _) end
 
 local function on_init(client, _)
