f8564680215eb5ab1e74432720407fcfb866addc
Lukas Leeb <git@lukasl.dev>
2024-10-22 00:26:32 +0200

    Add nvim plugins

---
+++ dots/nvim/lua/plugins/git-conflict.lua
@@ -0,0 +1,6 @@
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  config = true,
+  opts = {},
+}
--- dots/nvim/lua/plugins/todo-comments.lua
+++ dots/nvim/lua/plugins/todo-comments.lua
@@ -4,5 +4,35 @@ return {
 
   event = "BufWinEnter",
 
-  opts = {},
+  opts = {
+    keywords = {
+      FIX = {
+        icon = " ",
+        color = "error",
+        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+      },
+      TODO = { icon = " ", color = "info" },
+      HACK = { icon = " ", color = "warning" },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TEST = {
+        icon = "⏲ ",
+        color = "test",
+        alt = { "TESTING", "PASSED", "FAILED" },
+      },
+    },
+    search = {
+      command = "rg",
+      args = {
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "-i",
+      },
+      pattern = [[\b(KEYWORDS)\b]],
+    },
+  },
 }
