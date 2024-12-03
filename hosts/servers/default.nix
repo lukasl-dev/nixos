b0dd492b168fb0c75fda0ad5ee96ffb030be2c6f
Lukas Leeb <git@lukasl.dev>
2024-12-03 22:03:06 +0100

    Update sudo config

--- hosts/servers/default.nix
+++ hosts/servers/default.nix
@@ -18,4 +18,11 @@
   home-manager.users.lukas = lib.mkDefault (
     import ../../home/headless { inherit inputs pkgs pkgs-unstable; }
   );
+
+  security.sudo = {
+    enable = true;
+    extraConfig = ''
+      %wheel ALL=(ALL) NOPASSWD: ALL
+    '';
+  };
 }
