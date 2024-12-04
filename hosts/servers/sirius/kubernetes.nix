eaf68cb424ac01477c0e5fe60e3b7b24cb974b48
Lukas Leeb <git@lukasl.dev>
2024-12-04 02:27:36 +0100

    Switch to k3s

--- hosts/servers/sirius/kubernetes.nix
+++ hosts/servers/sirius/kubernetes.nix
@@ -1,38 +1,8 @@
-{ pkgs, ... }:
+{ config, ... }:
 
-let
-  kubeMasterAddr = "75.119.143.38";
-  kubeMasterHostname = "api.kube";
-  kubeMasterAPIServerPort = 6443;
-in
 {
-  # resolve master hostname
-  networking.extraHosts = "${kubeMasterAddr} ${kubeMasterHostname}";
-
-  # packages for administration tasks
-  environment.systemPackages = with pkgs; [
-    kompose
-    kubectl
-    kubernetes
-  ];
-
-  services.kubernetes = {
-    roles = [
-      "master"
-      "node"
-    ];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    easyCerts = true;
-    apiserver = {
-      securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterAddr;
-    };
-
-    # use coredns
-    addons.dns.enable = true;
-
-    # needed if you use swap
-    kubelet.extraOpts = "--fail-swap-on=false";
+  services.k3s = {
+    enable = true;
+    tokenFile = config.sops.secrets."k8s/token".path;
   };
 }
