0cc7bc915eed3029802bbf67f03e36d213b89f38
lukasl-dev <git@lukasl.dev>
2024-12-15 22:30:29 +0100

    Fix avante issue

--- home/default.nix
+++ home/default.nix
@@ -20,4 +20,10 @@
     enable = true;
     flavor = "mocha";
   };
+
+  home.file.".ssh/id_ed25519.pub" = {
+    enable = true;
+    source = ../dots/ssh/id_ed25519.pub;
+    target = ".ssh/id_ed25519.pub";
+  };
 }
