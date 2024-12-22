e8b7c01d053c4b4d0aa7b29ed32216c590ed1e75
lukasl-dev <git@lukasl.dev>
2024-12-22 17:46:19 +0100

    Add nix-community binary cache

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -21,8 +21,14 @@
     ];
 
     # binary cache
-    substituters = [ "https://nix.lukasl.dev" ];
-    trusted-public-keys = [ "nix.lukasl.dev:tKOhI7ckbT2uexK4/surblHqzPLHWf3mkm+87dewlu0=%" ];
+    substituters = [
+      "https://nix.lukasl.dev"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix.lukasl.dev:tKOhI7ckbT2uexK4/surblHqzPLHWf3mkm+87dewlu0=%"
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   nixpkgs.config = {
