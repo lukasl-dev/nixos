172910f4b70fc767dd22c639be841dfb45014b94
lukasl-dev <git@lukasl.dev>
2025-05-10 13:19:40 +0200

    Fix blink build issue

--- dots/nvim/lua/plugins/blink.lua
+++ dots/nvim/lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
 
   version = "1.*",
-  build = "nix run .#build-plugin",
+  build = "nix run --accept-flake-config .#build-plugin",
 
   dependencies = { "rafamadriz/friendly-snippets" },
 
