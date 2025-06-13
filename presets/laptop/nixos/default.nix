ab493d44ec0baddc80930b43a1ed8ef2d73c982f
lukasl-dev <git@lukasl.dev>
2025-06-13 03:27:08 +0200

    Add brightnessctl for laptops

--- presets/laptop/nixos/default.nix
+++ presets/laptop/nixos/default.nix
@@ -1,8 +1,14 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../../desktop/nixos
 
     ../../../nixosModules/system/thermald.nix
     ../../../nixosModules/system/tlp.nix
+  ];
+
+  environment.systemPackages =  [
+    pkgs.brightnessctl
   ];
 }
