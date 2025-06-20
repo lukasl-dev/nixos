0a7716f057dcd8bceb00416df2316d96123d5724
lukasl-dev <git@lukasl.dev>
2025-06-20 21:52:02 +0200

    Update min-quantum for pipewire

--- nixosModules/system/sound/pipewire.nix
+++ nixosModules/system/sound/pipewire.nix
@@ -4,10 +4,19 @@
   services.pipewire = {
     enable = true;
 
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
     pulse.enable = true;
     jack.enable = true;
+
+    extraConfig.pipewire = {
+      "context.properties" = {
+        "default.clock.min-quantum" = 2048;
+      };
+    };
   };
 
   services.libinput.enable = true;
