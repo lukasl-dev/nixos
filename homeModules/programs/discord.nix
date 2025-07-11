debe985d2e6782c2920fe6c3e7f134dc3098da69
lukasl-dev <git@lukasl.dev>
2025-07-11 02:50:46 +0200

    Simplify vesktop settings

--- homeModules/programs/discord.nix
+++ homeModules/programs/discord.nix
@@ -1,23 +1,95 @@
 { pkgs-unstable, ... }:
 
 {
-  home = {
-    packages = [
-      pkgs-unstable.vesktop
-      pkgs-unstable.discord
-    ];
+  programs.vesktop = {
+    enable = true;
+    package = pkgs-unstable.vesktop;
 
-    file = {
-      ".config/vesktop/settings.json" = {
-        enable = true;
-        source = ../../dots/vesktop/settings.json;
-        target = ".config/vesktop/settings.json";
+    vencord.settings = {
+      autoUpdate = true;
+      autoUpdateNotification = true;
+      useQuickCss = true;
+      themeLinks = [
+        "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css"
+      ];
+      enabledThemes = [ ];
+      enableReactDevtools = false;
+      frameless = false;
+      transparent = false;
+      winCtrlQ = false;
+      disableMinSize = false;
+      winNativeTitleBar = false;
+      plugins = {
+        ChatInputButtonAPI.enabled = true;
+        CommandsAPI.enabled = true;
+        MessageAccessoriesAPI.enabled = true;
+        MessageEventsAPI.enabled = true;
+        CrashHandler.enabled = true;
+        FakeNitro = {
+          enabled = true;
+          enableEmojiBypass = true;
+          emojiSize = 48;
+          transformEmojis = true;
+          enableStickerBypass = true;
+          stickerSize = 160;
+          transformStickers = true;
+          transformCompoundSentence = false;
+          enableStreamQualityBypass = false;
+          useHyperLinks = true;
+          hyperLinkText = "{{NAME}}";
+          disableEmbedPermissionCheck = false;
+        };
+        NoDevtoolsWarning.enabled = true;
+        SilentTyping = {
+          enabled = true;
+          isEnabled = true;
+          showIcon = false;
+        };
+        TypingIndicator = {
+          enabled = true;
+          includeMutedChannels = false;
+          includeCurrentChannel = true;
+        };
+        TypingTweaks = {
+          enabled = true;
+          alternativeFormatting = true;
+        };
+        WebKeybinds.enabled = true;
+        WebScreenShareFixes.enabled = true;
+        NoTrack = {
+          enabled = true;
+          disableAnalytics = true;
+        };
+        WebContextMenus = {
+          enabled = true;
+          addBack = true;
+        };
+        Settings = {
+          enabled = true;
+          settingsLocation = "aboveNitro";
+        };
+        SupportHelper.enabled = true;
+        BadgeAPI.enabled = true;
       };
-      ".config/vesktop/settings/settings.json" = {
-        enable = true;
-        source = ../../dots/vesktop/settings/settings.json;
-        target = ".config/vesktop/settings/settings.json";
+      notifications = {
+        timeout = 5000;
+        position = "bottom-right";
+        useNative = "not-focused";
+        logLimit = 50;
       };
+      cloud = {
+        authenticated = false;
+        url = "https://api.vencord.dev/";
+        settingsSync = false;
+        settingsSyncVersion = 1739212854099;
+      };
+    };
+
+    settings = {
+      checkUpdates = true;
+      arRPC = false;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
     };
   };
 }
