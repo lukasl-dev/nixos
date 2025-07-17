b6efcc1a66911ca4a220b4dab2a95f481686a298
lukasl-dev <git@lukasl.dev>
2025-07-17 15:45:24 +0200

    Add mini.nvim

---
+++ dots/nvim/lua/plugins/mini.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.nvim",
+
+  event = "VeryLazy",
+
+  config = function()
+    require("mini.ai").setup()
+    require("mini.operators").setup()
+    require("mini.surround").setup()
+  end,
+}
