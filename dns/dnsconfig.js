5bdd59ecc06a678554a098ce86d6a632d1168f4f
lukasl-dev <git@lukasl.dev>
2025-07-27 07:12:14 +0200

    Update dnsconfig.js

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -13,35 +13,29 @@ D(
 
 	// nodes
 	A("pollux.nodes", "185.245.61.227"),
-  AAAA("pollux.nodes", "2a13:7e80:0:b2::"),
+	AAAA("pollux.nodes", "2a13:7e80:0:b2::"),
 
 	// github pages
 	ALIAS("@", "lukasl-dev.github.io."),
 
 	// services
 	CNAME("anki", "pollux.nodes.lukasl.dev."),
-	CNAME("api.stack-auth", "pollux.nodes.lukasl.dev."),
-	CNAME("ascii", "sirius.nodes.lukasl.dev."),
-	CNAME("auth", "sirius.nodes.lukasl.dev."),
 	CNAME("cloud", "pollux.nodes.lukasl.dev."),
 	CNAME("git", "pollux.nodes.lukasl.dev."),
-	CNAME("mail", "pollux.nodes.lukasl.dev."),
 	CNAME("notes", "lukasl-dev.github.io."),
 	CNAME("proxy", "pollux.nodes.lukasl.dev."),
 	CNAME("vault", "pollux.nodes.lukasl.dev."),
 
-  // iconolatry
+	// iconolatry
 	CNAME("icons", "cname.vercel-dns.com."),
 
 	// mail
+	CNAME("mail", "pollux.nodes.lukasl.dev."),
 	MX("@", 10, "mail.lukasl.dev."),
-  TXT("@", "v=spf1 a:mail.lukasl.dev -all"),
-	TXT(
-		"_dmarc",
-		"v=DMARC1; p=none; rua=mailto:4391ec514dd240209efa9f49a0c60f7c@dmarc-reports.cloudflare.net",
-	),
+	TXT("@", "v=spf1 a:mail.lukasl.dev -all"),
+	TXT("_dmarc", "v=DMARC1; p=none"),
 
-  // social auth
+	// social auth
 	TXT("_atproto", "did=did:plc:hj7h5xqaqhtlinjcz4q2dclk"),
 	TXT("_discord", "dh=a03d403086eab384eb683b58c4360507b7f83238"),
 
