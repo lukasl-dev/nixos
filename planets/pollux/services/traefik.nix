9ee117080647556e181033ca675e6390e8acc5ca
lukasl-dev <git@lukasl.dev>
2025-08-21 01:16:03 +0200

    Use NixOS acme instead of traefik acme

--- planets/pollux/services/traefik.nix
+++ planets/pollux/services/traefik.nix
@@ -3,14 +3,14 @@
 let
   domain = config.universe.domain;
 
+  acmeDir = config.security.acme.certs.${domain}.directory;
+
   httpPort = 80;
   httpsPort = 443;
 in
 {
   services.traefik = {
     enable = true;
-
-    environmentFiles = [ (config.sops.templates."traefik/env".path) ];
 
     staticConfigOptions = {
       api.dashboard = true;
@@ -26,41 +26,22 @@ in
 
         websecure = {
           address = ":${toString httpsPort}";
-          http.tls = {
-            certResolver = "cloudflare";
-            domains = [
-              {
-                main = "${domain}";
-                sans = [ "*.${domain}" ];
-              }
-            ];
-          };
+          http.tls = { };
         };
       };
 
       log = {
         level = "DEBUG";
       };
-
-      certificatesResolvers = {
-        cloudflare = {
-          acme = {
-            email = "contact@${domain}";
-            storage = "${config.services.traefik.dataDir}/acme.json";
-            dnsChallenge = {
-              provider = "cloudflare";
-              resolvers = [
-                "1.1.1.1:53"
-                "1.0.0.1:53"
-              ];
-            };
-            # caServer = "https://acme-staging-v02.api.letsencrypt.org/directory";
-          };
-        };
-      };
     };
 
     dynamicConfigOptions = {
+      tls.certificates = [
+        {
+          certFile = "${acmeDir}/fullchain.pem";
+          keyFile = "${acmeDir}/key.pem";
+        }
+      ];
       http.routers = {
         dashboard = {
           rule = "Host(`proxy.${domain}`)";
@@ -79,14 +60,5 @@ in
     465
   ];
 
-  sops.secrets = {
-    "universe/cloudflare/email" = { };
-    "universe/cloudflare/account_id" = { };
-    "universe/cloudflare/global_api_key" = { };
-  };
-
-  sops.templates."traefik/env".content = ''
-    CF_API_EMAIL=${config.sops.placeholder."universe/cloudflare/email"}
-    CF_API_KEY=${config.sops.placeholder."universe/cloudflare/global_api_key"}
-  '';
+  users.users.traefik.extraGroups = [ "acme" ];
 }
