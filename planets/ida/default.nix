c49b465791dc2a8bd7c523e442e5b3169f5d3866
lukasl-dev <git@lukasl.dev>
2025-10-10 21:40:58 +0200

    Fix deadnix issues

--- planets/ida/default.nix
+++ planets/ida/default.nix
@@ -9,11 +9,9 @@
   # Match the working Pi4 example: let the initrd module closure tolerate
   # modules that the RPi kernel does not ship (avoids shrink failures).
   nixpkgs.overlays = [
-    (
-      final: super: {
-        makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
-      }
-    )
+    (_: super: {
+      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+    })
   ];
 
   planet = {
