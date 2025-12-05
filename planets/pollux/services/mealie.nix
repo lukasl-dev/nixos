1f9b9956b37f7a8d445dbbbaf1dc15044a9e0032
lukasl-dev <git@lukasl.dev>
2025-12-05 15:25:02 +0100

    Fix permissions

--- planets/pollux/services/mealie.nix
+++ planets/pollux/services/mealie.nix
@@ -9,11 +9,31 @@ in
     package = pkgs-unstable.mealie;
 
     port = 1989;
+    credentialsFile = config.sops.templates."planets/pollux/mealie/credentials".path;
 
     settings = {
       ALLOW_SIGNUP = "false";
       TZ = "Europe/Vienna";
+      BASE_URL = "https://kitchen.${domain}";
+
+      SMTP_HOST = "mail.${domain}";
+      SMTP_PORT = "587";
+      SMTP_FROM_NAME = "Mealie";
+      SMTP_AUTH_STRATEGY = "TLS";
+      SMTP_FROM_EMAIL = "bot@${domain}";
+      SMTP_USER = "bot@${domain}";
     };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/private/mealie 0750 mealie mealie - -"
+    "Z /var/lib/private/mealie 0750 mealie mealie - -"
+  ];
+
+  sops.templates."planets/pollux/mealie/credentials" = {
+    content = ''
+      SMTP_PASSWORD=${config.sops.placeholder."planets/pollux/maddy/bot"}
+    '';
   };
 
   services.traefik.dynamicConfigOptions.http = {
