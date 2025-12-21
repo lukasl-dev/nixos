f324b37a2c44c47a901ea63d3fd3e1dd41bc2168
lukasl-dev <git@lukasl.dev>
2025-12-21 01:09:57 +0100

    Fix git aliases

--- options/universe/programs/git.nix
+++ options/universe/programs/git.nix
@@ -59,7 +59,7 @@ in
             settings = {
               user = { inherit (git.user) name email; };
 
-              aliases = {
+              alias = {
                 graph = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
               };
 
