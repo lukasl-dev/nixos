761fa5a3629c4f41c3c04753ca231c4f85e47691
lukasl-dev <git@lukasl.dev>
2025-12-26 18:45:08 +0100

    Fix fff

--- packages/vim/plugins/fff.nix
+++ packages/vim/plugins/fff.nix
@@ -1,24 +1,34 @@
 { rinputs, pkgs, ... }:
 
 {
-  vim.lazy.plugins."fff.nvim" = {
+  vim.extraPlugins."fff.nvim" = {
     package = rinputs.fff-nvim.packages.${pkgs.stdenv.system}.fff-nvim;
-    setupOpts = {
-      prompt = "> ";
-    };
-    keys = [
-      {
-        key = "<leader>ff";
-        mode = [ "n" ];
-        lua = true;
-        action = # lua
-          ''
-            function()
-              require("fff").find_files()
-            end
-          '';
-        desc = "Files [FFF]";
-      }
-    ];
+    setup = # lua
+      ''
+        require("fff").setup {
+          prompt = '> ',
+          title = 'Find files',
+          max_threads = 8,
+          preview = {
+              line_numbers = true,
+              show_file_info = false,
+          },
+        }
+      '';
   };
+
+  vim.keymaps = [
+    {
+      key = "<leader>ff";
+      mode = [ "n" ];
+      lua = true;
+      action = # lua
+        ''
+          function()
+            require("fff").find_files()
+          end
+        '';
+      desc = "Files [FFF]";
+    }
+  ];
 }
