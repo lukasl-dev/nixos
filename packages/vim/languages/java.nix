1d44e0a7a17754beeae944bc1b00046e5273bc74
lukasl-dev <git@lukasl.dev>
2025-12-28 04:53:22 +0100

    Setup java language properly

--- packages/vim/languages/java.nix
+++ packages/vim/languages/java.nix
@@ -1,47 +1,19 @@
-# let
-# Wrapper that chooses available IntelliJ CLI (ultimate > community),
-# then runs the CLI formatter with all passed args (file path etc.).
-# intellijFormat = pkgs.writeShellApplication {
-#   name = "intellij-format";
-#   text = ''
-#     set -euo pipefail
-#     IDE="${pkgs.jetbrains.idea-community-bin}/bin/idea-community"
-#     if command -v idea-ultimate >/dev/null 2>&1; then
-#       IDE="idea-ultimate"
-#     elif command -v idea-community >/dev/null 2>&1; then
-#       IDE="idea-community"
-#     fi
-#     exec "$IDE" format -allowDefaults "$@"
-#   '';
-# };
-# in
+{ pkgs, ... }:
+
+let
+  jdtls-lombok = pkgs.writeShellScriptBin "jdtls" ''
+    exec ${pkgs.jdt-language-server}/bin/jdtls --jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar "$@"
+  '';
+in
 {
   vim = {
-    # extraPackages = [ intellijFormat ];
+    extraPackages = with pkgs; [ google-java-format ];
 
-    languages.java.enable = true;
+    languages.java = {
+      enable = true;
+      lsp.package = jdtls-lombok;
+    };
 
-    # lsp.servers.jdtls.settings = {
-    #   java = {
-    #     project = {
-    #       sourcePaths = [ "." ];
-    #     };
-    #   };
-    # };
-
-    # formatter.conform-nvim = {
-    #   setupOpts = {
-    #     formatters = {
-    #       intellij_format = {
-    #         command = "${intellijFormat}/bin/intellij-format";
-    #         args = [ "$FILENAME" ];
-    #         stdin = false;
-    #         # Allow enough time for the JVM to start
-    #         timeout_ms = 60000;
-    #       };
-    #     };
-    #     formatters_by_ft.java = [ "intellij_format" ];
-    #   };
-    # };
+    formatter.conform-nvim.setupOpts.formatters_by_ft.java = [ "google-java-format" ];
   };
 }
