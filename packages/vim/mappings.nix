8c88e1ff8b191595de09f97320ae312a7b7bdc89
lukasl-dev <git@lukasl.dev>
2026-01-24 15:32:15 +0100

    Add mappings

--- packages/vim/mappings.nix
+++ packages/vim/mappings.nix
@@ -8,11 +8,25 @@
       desc = "Move line down";
     }
     {
+      mode = "v";
+      key = "<C-j>";
+      action = ":m '>+1<CR>gv=gv";
+      silent = true;
+      desc = "Move lines up";
+    }
+    {
       mode = "n";
       key = "<C-k>";
       action = ":m .-2<CR>==";
       silent = true;
       desc = "Move line up";
+    }
+    {
+      mode = "v";
+      key = "<C-k>";
+      action = ":m '<-2<CR>gv=gv";
+      silent = true;
+      desc = "Move lines up";
     }
   ];
 }
