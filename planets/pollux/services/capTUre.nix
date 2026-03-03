a463bb2b5a686bb070ee2a72ecc1cdf51e41fd73
lukasl-dev <git@lukasl.dev>
2026-03-03 23:23:29 +0100

    Update capTUre

--- planets/pollux/services/capTUre.nix
+++ planets/pollux/services/capTUre.nix
@@ -5,63 +5,35 @@
 
   services.capTUre = {
     enable = false;
-    schedule = {
-      mo_08_stats = {
-        weekday = "Monday";
-        start = "08:00";
+    weekly = {
+      tu_09_ea_quer = {
+        weekday = "Tuesday";
+        start = "09:00";
         duration = 2 * 60;
-        channel = "informatikhoersaal";
+        channel = "ei8";
       };
-      mo_10_swe = {
-        weekday = "Monday";
+      th_10_ea_quer = {
+        weekday = "Thursday";
         start = "10:00";
         duration = 2 * 60;
-        channel = "informatikhoersaal";
+        channel = "ei8";
       };
-      mo_12_thinf = {
-        weekday = "Monday";
-        start = "12:00";
+    };
+    dates = {
+      tu_2026_03_03_09_first_lecture = {
+        start = "2026-03-03 09:00:00";
         duration = 2 * 60;
-        channel = "informatikhoersaal";
+        channel = "ei8";
       };
-      mo_14_pp = {
-        weekday = "Monday";
-        start = "14:00";
-        duration = 2 * 60;
-        channel = "informatikhoersaal";
-      };
-
-      tu_13_os = {
-        weekday = "Tuesday";
-        start = "13:00";
-        duration = 2 * 60;
-        channel = "informatikhoersaal";
-      };
-      tu_15_os_ue = {
-        weekday = "Tuesday";
-        start = "15:00";
-        duration = 2 * 60;
-        channel = "informatikhoersaal";
-      };
-
-      wed_12_iml = {
-        weekday = "Wednesday";
-        start = "12:00";
+      fr_2026_03_06_10_hv_inf = {
+        start = "2026-03-06 10:00:00";
         duration = 2 * 60;
         channel = "fav1";
       };
-      wed_13_thinf = {
-        weekday = "Wednesday";
-        start = "13:00";
+      fr_2026_03_13_10_hv_inf = {
+        start = "2026-03-13 10:00:00";
         duration = 2 * 60;
-        channel = "informatikhoersaal";
-      };
-
-      th_15_os_ue = {
-        weekday = "Thursday";
-        start = "15:00";
-        duration = 2 * 60;
-        channel = "informatikhoersaal";
+        channel = "fav1";
       };
     };
   };
