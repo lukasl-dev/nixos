d07770207ddf0c60c6ea1c20491fad125bcd9738
lukasl-dev <git@lukasl.dev>
2026-03-03 22:12:48 +0100

    Open pihole firewall

--- planets/ida/services/pihole.nix
+++ planets/ida/services/pihole.nix
@@ -6,53 +6,57 @@ let
   webPort = 2718;
 in
 {
-  services.resolved.extraConfig = ''
-    DNSStubListener=no
-  '';
+  services = {
+    resolved.extraConfig = ''
+      DNSStubListener=no
+    '';
 
-  services.pihole-ftl = {
-    enable = true;
+    pihole-ftl = {
+      enable = true;
 
-    settings = {
-      dns = {
-        listeningMode = "ALL";
-        upstreams = [
-          "1.1.1.1"
-          "1.0.0.1"
-          "8.8.8.8"
-          "8.8.4.4"
-        ];
+      openFirewallDNS = true;
+
+      settings = {
+        dns = {
+          listeningMode = "ALL";
+          upstreams = [
+            "1.1.1.1"
+            "1.0.0.1"
+            "8.8.8.8"
+            "8.8.4.4"
+          ];
+        };
       };
+
+      lists = [
+        {
+          url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+          type = "block";
+          enabled = true;
+          description = "hagezi blocklist";
+        }
+      ];
     };
 
-    lists = [
-      {
-        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
-        type = "block";
-        enabled = true;
-        description = "hagezi blocklist";
-      }
-    ];
-  };
-
-  services.pihole-web = {
-    enable = true;
-    hostName = host;
-    ports = [ webPort ];
-  };
-
-  services.traefik.dynamicConfigOptions.http = {
-    routers.pihole = {
-      rule = "Host(`${host}`)";
-      entryPoints = [ "websecure" ];
-      service = "pihole";
-      tls = { };
+    pihole-web = {
+      enable = true;
+      hostName = host;
+      ports = [ webPort ];
     };
 
-    services.pihole.loadBalancer.servers = [
-      {
-        url = "http://127.0.0.1:${toString webPort}";
-      }
-    ];
+    traefik.dynamicConfigOptions.http = {
+      routers.pihole = {
+        rule = "Host(`${host}`)";
+        entryPoints = [ "websecure" ];
+        service = "pihole";
+        tls = { };
+      };
+
+      services.pihole.loadBalancer.servers = [
+        {
+          url = "http://127.0.0.1:${toString webPort}";
+        }
+      ];
+    };
   };
 }
