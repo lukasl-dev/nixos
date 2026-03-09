1f2a96949ef75a28fb683d5dbbba671bdc8d583c
lukasl-dev <git@lukasl.dev>
2026-03-09 04:22:08 +0100

    Armor upterm

--- options/universe/upterm.nix
+++ options/universe/upterm.nix
@@ -6,6 +6,7 @@
 }:
 
 let
+  inherit (config.universe) user;
   inherit (pkgs.stdenv.hostPlatform) system;
 
   package = self.packages.${system}.upterm;
@@ -23,5 +24,40 @@ let
   };
 in
 {
+  security.apparmor.policies.upterm = {
+    state = "enforce";
+    profile = ''
+      abi <abi/4.0>,
+      include <tunables/global>
+
+      profile upterm "${wrapped}/bin/upterm" {
+        include <abstractions/base>
+
+        allow all,
+
+        audit deny "${pkgs.unstable.sops}/bin/sops" x,
+        audit deny "${pkgs.sops}/bin/sops" x,
+        audit deny "/etc/sops/age/**" rwklm,
+        audit deny "/etc/sops/**" rwklm,
+
+        audit deny "/etc/agenix/identity" rwklm,
+        audit deny "/etc/agenix/**" rwklm,
+
+        audit deny "/home/${user.name}/nixos/dns/creds.json" rwklm,
+
+        audit deny "/run/secrets/" r,
+        audit deny "/run/secrets/**" r,
+
+        audit deny "/run/agenix/" r,
+        audit deny "/run/agenix/**" r,
+        audit deny "/run/agenix.d/" r,
+        audit deny "/run/agenix.d/**" r,
+
+        audit deny "/home/${user.name}/nixos/secrets/**" rwklm,
+        audit deny "/home/${user.name}/nixos/sops/**" rwklm,
+      }
+    '';
+  };
+
   environment.systemPackages = [ wrapped ];
 }
