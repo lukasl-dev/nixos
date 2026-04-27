874b7473af1717c585a8c455587b1d32b303d1be
lukasl-dev <git@lukasl.dev>
2026-04-27 18:25:07 +0200

    Use pi-openai extension from github

--- options/universe/programs/pi-mono/default.nix
+++ options/universe/programs/pi-mono/default.nix
@@ -68,6 +68,13 @@ let
     rev = "a6839e57c0f0d8d534b01e646abce2d6530faf01";
     hash = "sha256-ecS05kVnga1y+OoRoUH7/+WCrQsxgP/q/AcSWAPyO8o=";
   };
+
+  pi-openai = pkgs.fetchFromGitHub {
+    owner = "lukasl-dev";
+    repo = "pi-openai";
+    rev = "14b34130d219158762a27b460d6cc85a667a622e";
+    hash = "sha256-Ivxl8/UaEiZtqQA4uM355IVjmgzh2doTdQuXQsfxeVg=";
+  };
 in
 {
   imports = [ inputs.pi-mono.nixosModules.default ];
@@ -135,11 +142,11 @@ in
     ];
 
     extensions = [
-      ./extensions/openai.ts
       ./extensions/wakatime.ts
       "${pi-thinking-steps}"
       "${pi-fff}/packages/pi-fff"
       "${pi-usage-extension}/usage-extension"
+      "${pi-openai}"
     ];
 
     themes = [ ./themes/catppuccin-mocha.json ];
