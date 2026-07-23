02fa1d435a2f31954b8dff4dad128396b5b0aa3f
lukasl-dev <git@lukasl.dev>
2026-07-23 06:04:43 +0200

    Port

--- modules/traveller/jujutsu.nix
+++ modules/traveller/jujutsu.nix
@@ -8,20 +8,17 @@ in
     (
       { pkgs, ... }:
 
-      let
-        configFile = (pkgs.formats.toml { }).generate "jujutsu-config.toml" {
-          user = {
-            name = traveller.git.user;
-            inherit (traveller) email;
-          };
-
-          ui.default-command = "log";
-        };
-      in
       {
         hjem.users.${traveller.user.name} = {
           packages = [ pkgs.jujutsu ];
-          xdg.config.files."jj/config.toml".source = configFile;
+          xdg.config.files."jj/config.toml".source = (pkgs.formats.toml { }).generate "jujutsu-config.toml" {
+            user = {
+              name = traveller.git.user;
+              inherit (traveller) email;
+            };
+
+            ui.default-command = "log";
+          };
         };
       }
     )
