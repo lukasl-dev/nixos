c3138ac02c00570b237b74fc8d765123b8f5bdf9
lukasl-dev <me@lukasl.dev>
2026-07-28 17:51:23 +0200

    Setup jellyfin

--- modules/planet/hosting/media.nix
+++ modules/planet/hosting/media.nix
@@ -9,9 +9,20 @@ let
   inherit (config.planet.hosting) media;
   inherit (atlas.hosting.media) host;
 
+  steward = atlas.travellers.eval config.planet.steward.traveller;
+
   listenAddress = "127.0.0.1";
   port = 8096;
   dataDir = "/var/lib/jellyfin";
+
+  mediaDir = "/srv/media";
+  libraryDirs = map (library: "${mediaDir}/${library}") [
+    "lectures"
+    "movies"
+    "music"
+    "photos"
+    "shows"
+  ];
 in
 {
   options.planet.hosting.media.enable =
@@ -23,8 +34,19 @@ in
       inherit dataDir;
     };
 
+    users = {
+      groups.media = { };
+      users.jellyfin.extraGroups = [ "media" ];
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${mediaDir} 2750 ${steward.user.name} media - -"
+    ]
+    ++ map (dir: "d ${dir} 2750 ${steward.user.name} media - -") libraryDirs;
+
     planet = {
       backup.dirs = [ dataDir ];
+      steward.groups = [ "media" ];
 
       hosting.proxy.rules.media = {
         ingress.host = host;
